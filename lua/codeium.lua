48a119433742c9a5c5d92c1fd605b5a5540b87ab
firefoxmmx <firefoxmmx@163.com>
2023-03-23 21:54:43 +0800

    修正codeium的lua版本的配置，现在可以正常使用了

--- lua/codeium.lua
+++ lua/codeium.lua
@@ -1,12 +1,15 @@
 -- codeium settings
 --
 -- codeium airline#SetStatus setting
+-- 简化fn vim函数符号
+local fn = vim.fn
 function GetCodeiumStatus()
-  return '{…}' .. codeium.GetStatusString()
+  -- lua里字符串连接符号使用两个点也就是..来操作
+  return '{…}' .. vim.fn['codeium#GetStatusString']()
 end
-
-vim.fn['airline#parts#define_function']('codeium', GetCodeiumStatus)
-vim.fn['airline#parts#define_condition']('codeium', vim.fn.exists('*codeium#GetStatusString'))
-vim.g.airline_section_x = vim.fn['airline#section#create_right']({ 'codeium', 'filetype' })
+-- 在vim环境里面调用lua的函数使用v:lua.的环境前缀
+fn['airline#parts#define_function']('codeium', 'v:lua.GetCodeiumStatus')
+fn['airline#parts#define_condition']('codeium', "exists('*codeium#GetStatusString')")
+vim.g.airline_section_x = fn['airline#section#create_right']({ 'codeium', 'filetype' })
 -- global enable codeium
 vim.g.codeium_enabled = true
