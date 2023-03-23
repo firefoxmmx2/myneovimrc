59fa7da2e2ec21b9a7694eb9a1ecb020788945d5
firefoxmmx <firefoxmmx@163.com>
2023-03-23 22:54:20 +0800

    新增一个diffview插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,4 +35,5 @@ return  require('packer').startup(function()
   use 'Exafunction/codeium.vim'
   use 'skanehira/docker-compose.vim'
   use 'ggandor/leap.nvim'
+  use { 'sindrets/diffview.nvim', requires = 'nvim-lua/plenary.nvim' }
 end)
