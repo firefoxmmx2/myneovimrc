65b6b9f50e22a99f8a089a3da7924cb8f4e3f05c
huxin <huxin@aisino.com>
2023-04-27 17:29:17 +0800

    add fzf setting

--- lua/fzf.lua
+++ lua/fzf.lua
@@ -2,3 +2,4 @@
 vim.api.nvim_set_keymap('n', '<leader>b', ':Buffers<CR>', {silent = true})
 vim.api.nvim_set_keymap('n', '<space>f', ':Files<cr>', {silent = true})
 vim.api.nvim_set_keymap('n', '<space>h', ':History<cr>', {silent = true})
+vim.api.nvim_set_keymap('n', '<leader>fr', ':Rg ', {silent = true})
