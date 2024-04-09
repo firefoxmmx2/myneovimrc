12fe89c0c35295b6b150bac15f86ce2536695bb9
huxin <huxin@aisino.com>
2024-04-09 14:12:45 +0800

    add map alt + ; to open codeium chat

--- lua/codeium.lua
+++ lua/codeium.lua
@@ -5,7 +5,7 @@
 local fn = vim.fn
 function GetCodeiumStatus()
   -- lua里字符串连接符号使用两个点也就是..来操作
-  return '{…}' .. vim.fn['codeium#GetStatusString']()
+  return '{…}' .. fn['codeium#GetStatusString']()
 end
 -- 在vim环境里面调用lua的函数使用v:lua.的环境前缀
 fn['airline#parts#define_function']('codeium', 'v:lua.GetCodeiumStatus')
@@ -13,3 +13,5 @@ fn['airline#parts#define_condition']('codeium', "exists('*codeium#GetStatusStrin
 vim.g.airline_section_x = fn['airline#section#create_right']({ 'codeium', 'filetype' })
 -- global enable codeium
 vim.g.codeium_enabled = true
+-- keymap
+vim.keymap.set('n', '<a-;>', ':call codeium#Chat()<cr>')
