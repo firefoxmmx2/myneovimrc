85718342376012705efefc335c22fdc545891386
firefoxmmx <firefoxmmx@163.com>
2024-12-25 23:01:14 +0800

    set easymotion keymap

--- lua/easymotion.lua
+++ lua/easymotion.lua
@@ -1,10 +1,10 @@
 local keyset = vim.keymap.set
 
-keyset('n','s','<Plug>(easymotion-f2)')
-keyset('n','S','<Plug>(easymotion-F2)')
+keyset('n','s','<Plug>(easymotion-s)')
+-- keyset('n','S','<Plug>(easymotion-F2)')
 -- 智能匹配大小写
 vim.g.EasyMotion_smartcase = 1
 -- 可视模式可以使用s定位跳跃
-keyset('v','s','<Plug>(easymotion-f2)')
+keyset('v','s','<Plug>(easymotion-s)')
 -- 可视模式可以使用S定位跳跃
-keyset('v','S','<Plug>(easymotion-F2)')
+-- keyset('v','S','<Plug>(easymotion-F2)')
