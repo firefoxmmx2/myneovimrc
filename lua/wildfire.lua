81d0d51dd30ccb231265cd3fd1a81fbf98de0c64
huxin <huxin@aisino.com>
2025-01-22 18:09:33 +0800

    update windsurf setting

--- lua/wildfire.lua
+++ lua/wildfire.lua
@@ -1,2 +1,4 @@
 -- this setting for wildfire selection
 vim.keymap.set('n','<leader>s',[[<Plug>(wildfire-quick-select)]])
+vim.keymap.set('n','<cr>', '',{ noremap = true, silent = true })
+vim.keymap.set('n','<bs>', '',{ noremap = true, silent = true })
