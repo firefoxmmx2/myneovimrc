b414886f78d72bfc6c83ddb1a446d43cfd9606c6
huxin <huxin@aisino.com>
2026-03-10 11:33:13 +0800

    增加lazyvim版本的codeium插件。

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,35 +1,4 @@
 return {
   "Exafunction/windsurf.vim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    require("codeium").setup({
-      -- configuration
-      use({
-        "Exafunction/windsurf.vim",
-        config = function()
-          -- Change '<C-g>' here to any keycode you like.
-          vim.keymap.set("i", "<C-g>", function()
-            return vim.fn["codeium#Accept"]()
-          end, { expr = true, silent = true })
-          vim.keymap.set("i", "<c-;>", function()
-            return vim.fn["codeium#CycleCompletions"](1)
-          end, { expr = true, silent = true })
-          vim.keymap.set("i", "<c-,>", function()
-            return vim.fn["codeium#CycleCompletions"](-1)
-          end, { expr = true, silent = true })
-          vim.keymap.set("i", "<c-x>", function()
-            return vim.fn["codeium#Clear"]()
-          end, { expr = true, silent = true })
-        end,
-      }),
-    })
-  end,
+  event = "BufEnter",
 }
-
--- codeium settings
---
--- codeium airline#SetStatus setting
--- 简化fn vim函数符号
