44d796b1402acbbcc9219483a552b4ee859dc98a
huxin <huxin@aisino.com>
2026-03-11 10:17:52 +0800

    codeium删除无效的配置

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,11 +1,4 @@
 return {
   "Exafunction/windsurf.vim",
-<<<<<<< HEAD
   event = "BufEnter",
-=======
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
->>>>>>> cb61e9ef8fba82bbdea64623a5a556d543aaa557
 }
