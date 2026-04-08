048bc6ac416bcf8e918092634d0d67f3680fe259
firefoxmmx <firefoxmmx@163.com>
2026-04-08 23:20:04 +0800

    Wed Apr  8 11:20:04 PM CST 2026 auto commit

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -1,43 +1,41 @@
 return {
-  "nvim-mini/mini.nvim",
-  version = false,
-  opts = {
-    animate = {
+  "nvim-mini/mini.animate",
+  event = "VeryLazy",
+  cond = vim.g.neovide == nil,
+  opts = function(_, opts)
+    local animate = require("mini.animate")
+    
+    -- don't use animate when scrolling with the mouse
+    local mouse_scrolled = false
+    for _, scroll in ipairs({ "Up", "Down" }) do
+      local key = "<ScrollWheel" .. scroll .. ">"
+      vim.keymap.set({ "", "i" }, key, function()
+        mouse_scrolled = true
+        return key
+      end, { expr = true })
+    end
+    
+    return vim.tbl_deep_extend("force", opts, {
       cursor = {
         enable = true,
-        timing = {
-          duration = 150,
-          easing = function(t) return 1 - math.cos(t * math.pi / 2) end,
-        },
+        timing = animate.gen_timing.linear({ duration = 120, unit = "total" }),
       },
       scroll = {
         enable = true,
-        timing = {
-          duration = 200,
-          easing = function(t) return t * (2 - t) end,
-        },
+        timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
       },
       resize = {
         enable = true,
-        timing = {
-          duration = 150,
-          easing = function(t) return t * (2 - t) end,
-        },
+        timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
       },
       open = {
         enable = true,
-        timing = {
-          duration = 200,
-          easing = function(t) return t * (2 - t) end,
-        },
+        timing = animate.gen_timing.linear({ duration = 200, unit = "total" }),
       },
       close = {
         enable = true,
-        timing = {
-          duration = 150,
-          easing = function(t) return t * t end,
-        },
+        timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
       },
-    },
-  },
+    })
+  end,
 }
