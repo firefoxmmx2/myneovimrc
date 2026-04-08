c367734f86a631c5597094a16e2a42de8c8f09a6
firefoxmmx <firefoxmmx@163.com>
2026-04-09 00:05:04 +0800

    Thu Apr  9 12:05:04 AM CST 2026 auto commit

--- lua/plugins/mini-animate.lua
+++ lua/plugins/mini-animate.lua
@@ -4,27 +4,27 @@ return {
   cond = vim.g.neovide == nil,
   opts = function(_, opts)
     local animate = require("mini.animate")
-    
+
     return vim.tbl_deep_extend("force", opts, {
       cursor = {
         enable = true,
-        timing = animate.gen_timing.linear({ duration = 80, unit = "total" }),
+        timing = animate.gen_timing.linear({ duration = 250, unit = "total" }),
       },
       scroll = {
         enable = true,
-        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+        timing = animate.gen_timing.linear({ duration = 250, unit = "total" }),
       },
       resize = {
         enable = true,
-        timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
+        timing = animate.gen_timing.linear({ duration = 250, unit = "total" }),
       },
       open = {
         enable = true,
-        timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
+        timing = animate.gen_timing.linear({ duration = 250, unit = "total" }),
       },
       close = {
         enable = true,
-        timing = animate.gen_timing.linear({ duration = 100, unit = "total" }),
+        timing = animate.gen_timing.linear({ duration = 150, unit = "total" }),
       },
     })
   end,
