df50abd8314505889774b177ee39498378620dd2
huxin <huxin@aisino.com>
2026-04-16 16:45:04 +0800

    Thu Apr 16 04:45:04 PM CST 2026 auto commit

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Jenkinsfile syntax highlighting
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = { "Jenkinsfile", "Jenkinsfile.*" },
+  callback = function()
+    vim.opt.filetype = "groovy"
+  end,
+})
