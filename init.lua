33a6ed27e203c1547c958404a80aa3b1d8773ec6
firefoxmmx <firefoxmmx@163.com>
2026-04-18 23:40:04 +0800

    Sat Apr 18 11:40:04 PM CST 2026 auto commit

--- init.lua
+++ init.lua
@@ -1,2 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- 映射 ctrl+shift+u/d 和 shift+enter
+vim.keymap.set('n', '<C-S-u>', '<C-u>', { desc = 'Scroll up' })
+vim.keymap.set('n', '<C-S-d>', '<C-d>', { desc = 'Scroll down' })
+vim.keymap.set('i', '<C-S-u>', '<C-u>', { desc = 'Scroll up' })
+vim.keymap.set('i', '<C-S-d>', '<C-d>', { desc = 'Scroll down' })
+
+-- shift+enter 在插入模式下换行不退出
+vim.keymap.set('i', '<S-CR>', '<CR>', { desc = 'Enter' })
