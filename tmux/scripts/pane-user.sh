fe3c1ee1705f2803ddbe172990a9d7f7ca5e9dbc
firefoxmmx <firefoxmmx@163.com>
2026-04-26 22:30:05 +0800

    Sun Apr 26 10:30:05 PM CST 2026 auto commit

--- tmux/scripts/pane-user.sh
+++ tmux/scripts/pane-user.sh
@@ -7,4 +7,14 @@ if [ -n "$pane_id" ]; then
     tty=$(tmux display -p -t "$pane_id" '#{pane_tty}' 2>/dev/null)
     user=$(ps -t "$tty" -o euser=,stat= 2>/dev/null | awk '/\+/{print $1; exit}')
 fi
-echo "${user:-$(whoami)}"
+user="${user:-$(whoami)}"
+
+if [ "$user" = "root" ]; then
+    if [ $((10#$(date +%S) % 2)) -eq 0 ]; then
+        echo "#[fg=red,bg=#{@thm_surface_0}]#[fg=#{@thm_crust},bg=red] #[fg=red,bold,bg=#{@thm_surface_0}] root! #[fg=#{@thm_fg}]"
+    else
+        echo "#[fg=red,bg=#{@thm_surface_0}]#[fg=#{@thm_crust},bg=red] #[fg=red,bold,bg=#{@thm_surface_0}] root  #[fg=#{@thm_fg}]"
+    fi
+else
+    echo "#[fg=#{@thm_sky},bg=#{@thm_surface_0}]#[fg=#{@thm_crust},bg=#{@thm_sky}] #[fg=#{@thm_fg},bg=#{@thm_surface_0}] $user #[fg=#{@thm_fg}]"
+fi
