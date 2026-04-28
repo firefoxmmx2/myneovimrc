a880f7b93d3a1004be231d73d1aad999e3f36b4e
huxin <huxin@aisino.com>
2026-04-28 14:15:04 +0800

    Tue Apr 28 02:15:04 PM CST 2026 auto commit

--- tmux/scripts/pane-user.sh
+++ tmux/scripts/pane-user.sh
@@ -3,9 +3,10 @@ pane_id="${TMUX_PANE:-$1}"
 if [ -z "$pane_id" ]; then
     pane_id=$(tmux display -p '#{pane_id}' 2>/dev/null)
 fi
+user=""
 if [ -n "$pane_id" ]; then
     tty=$(tmux display -p -t "$pane_id" '#{pane_tty}' 2>/dev/null)
-    user=$(ps -t "$tty" -o euser=,stat= 2>/dev/null | awk '/\+/{print $1; exit}')
+    user=$(ps -t "$tty" -o euser:20=,stat= --no-headers 2>/dev/null | awk '/\+/{print $1; exit}')
 fi
 user="${user:-$(whoami)}"
 
