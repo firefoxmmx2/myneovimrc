f0c5ea2a069f157e8b4d36b95ae5e71b945b1311
huxin <huxin@aisino.com>
2026-07-09 23:40:06 +0800

    Thu Jul  9 11:40:06 PM CST 2026 auto commit

--- lua/plugins/sudo.lua
+++ lua/plugins/sudo.lua
@@ -5,6 +5,7 @@ return {
     { "<leader>w!!", desc = "Save as root", mode = "n" },
   },
   init = function()
+    vim.g.suda_smart_edit = 1
     vim.keymap.set("n", "<leader>w!!", ":SudaWrite<CR>", { desc = "Save as root" })
   end,
 }
